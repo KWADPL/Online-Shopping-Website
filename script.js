beac3cc18f3b53a134656e5461a35f6462c0a571
Karolina Wadecka <wadeckakarolina@gmail.com>
2024-01-19 00:45:59 +0100

    Shop and product page modeled- frontend

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ if(close) {
         nav.classList.remove('active')
     })
 }
+var MainImg = document.getElementById("MainImg")
+var smallimg = document.getElementsByClassName("small-img")
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
+}
+function rateProduct(selectedStars, groupId) {
+    const stars = document.querySelectorAll(`#${groupId} i`);
+
+    for (let i = 0; i < stars.length; i++) {
+        if (i < selectedStars) {
+            stars[i].style.color = 'gold';
+        } else {
+            stars[i].style.color = 'black';
+        }
+    }
+}
+
